3bec2c86d9defdb1c5276ab706b47b571bc628a7
Darren Sebastian <89560675+darrencelzo77@users.noreply.github.com>
2024-06-14 13:54:01 +0800

    s
    s

---
+++ admin/nav/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="container footer--flex">
+      <div class="footer-start">
+        <p>2024 © Clarence & Patrick <a href="javascript:void"></a>
+      </div>
+      <ul hidden class="footer-end">
+        <li hidden><a href="##">About</a></li>
+        <li hidden><a href="##">Support</a></li>
+        <li hidden><a href="##">Puchase</a></li>
+      </ul>
+    </div>
+  </footer>
---
+++ admin/nav/sidenav.php
@@ -0,0 +1,53 @@
+<aside class="sidebar">
+    <div class="sidebar-start">
+        <div class="sidebar-head">
+            <a href="home" class="logo-wrapper" title="Home">
+                <span class="sr-only">Home</span>
+                <!-- <span class="icon logo" aria-hidden="true"></span> -->
+                <div class="logo-text">
+                    <span class="logo-title">Admin</span>
+                    <span class="logo-subtitle">Dashboard</span>
+                </div>
+
+            </a>
+            <button class="sidebar-toggle transparent-btn" title="Menu" type="button">
+                <span class="sr-only">Toggle menu</span>
+                <span class="icon menu-toggle" aria-hidden="true"></span>
+            </button>
+        </div>
+        <div class="sidebar-body">
+            <ul class="sidebar-body-menu">
+                <span hidden class="system-menu__title">Menu</span>
+                <li>
+                    <a class="active" href="/"><span class="icon home" aria-hidden="true"></span>Dashboard</a>
+                </li>
+                <li>
+                    <a href="comments.html">
+                        <span class="icon message" aria-hidden="true"></span>
+                        Registration
+                    </a>
+                    <span hidden class="msg-counter">7</span>
+                </li>
+                <li>
+                    <a href="comments.html">
+                        <span class="icon message" aria-hidden="true"></span>
+                        Scholarship List
+                    </a>
+                    <span hidden class="msg-counter">7</span>
+                </li>
+                <li>
+                    <a href="comments.html">
+                        <span class="icon message" aria-hidden="true"></span>
+                        User
+                    </a>
+                </li>
+                 <li>
+                    <a href="comments.html">
+                        <span class="icon message" aria-hidden="true"></span>
+                        Category
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</aside>    
